38fb674166f8ad4cf7676a034d5c1987d813d282
WILLIAN VALENTIN <willianvalentin099@gmail.com>
2025-03-12 20:15:55 +0000

    c

--- olamundo.C
+++ olamundo.C
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+  // Definição da estrutura da carta
+  
+    char estado ['Paraná'];
+    char codigo[50]
+    char nome['Paraná'];
+    int populacao; 100.0000;
+    float area_km2; 330.000;
+    float pib; 69.000;
+    int pontos_turisticos; 50;
+
+    //Definição dos valores
+    printf("Estado: Paraná\n");
+    printf("Código: %d\n", codigo");
+
+      return ;0
+                                                                                                                                                                                                          
+
+  // Criando uma variável do tipo carta e atribuindo valores
